9d7e96ee980d9bee67bc7fad8c528bf2b1067b38
Abid Hasan <abidhasan@gmail.com>
2017-12-31 14:17:58 -0800

    Voice Servers basics complete

--- vatsim_helpers.py
+++ vatsim_helpers.py
@@ -3,28 +3,11 @@ Module contains helper functions for main.py for restful_vatsim
 '''
 import time, random, requests
 
-def update_file(current_file):
-    '''update_file() checks for data freshness in the local cache and if
-    data is too old, then it fetches new data. It then returns freshest data'''
-    #Check cache freshness; call download if needed
-    if (current_file["time_updated"] + 120) < int(time.time()):
-        #Needs updating
-        new_file = download()
-        if new_file is not None:
-            current_file["time_updated"] = int(time.time())
-            #Jsonify the data
-            current_file["data"] = jsonify_data(new_file)
-        else:
-            print "Downloaded file not valid"
-
-    #Return newest data
-    return current_file
-
 ################################################################################
 
 def download():
     '''download() tries to download the latest vatsim data from a list of
-    randomized servers'''
+    randomized servers to distribute server load'''
     #random urls to download from
     vatsim_urls = ["http://info.vroute.net/vatsim-data.txt", "http://data.vattastic.com/vatsim-data.txt", \
             "http://vatsim.aircharts.org/vatsim-data.txt", "http://vatsim-data.hardern.net/vatsim-data.txt", \
@@ -58,11 +41,38 @@ def check_line_validity(line):
         #This is an invalid line (non-pilot/ATC/voice server)
         if len(line) != 6 and len(line) != 42:
             return False
-    #Line is not accessible at 0th position, so must be empty to being with
+    #Line is not accessible at 0th position, so must be empty
     except IndexError:
         return False
-
+    #Line must be valid
     return True
+
+################################################################################
+
+def prettify_data(line, **kwargs):
+    ''' prettify_data() recieves a line of data from the vatsim file, already split,
+    and prettifies it to make it ready for jsonifying - discards unneeded data, while
+    keeping the data that the API needs to include. Returns a dictionary. Type =
+    "pilot", "controller" or "voice_servers" '''
+
+    if kwargs["type"] == "voice_servers":
+        return {"Location": line[0], "IP Address": line[1], "Name": line[2],
+        "Host Name": line[3], "Clients Allowed": line[4]}
+
+    elif kwargs["type"] == "controllers":
+        return {"Callsign": line[0], "Vatsim ID": line[1], \
+        "Real Name": line[2], "Frequency": line[4], "Latitude": line[5], \
+        "Longitude": line[6], "Visible Range": line[19], "ATIS": \
+        line[35], "Login Time": line[37]}
+
+    elif kwargs["type"] == "pilots":
+        return {"Callsign": line[0], "Vatsim ID": line[1], "Real Name": line[2], \
+        "Latitude": line[5], "Longitude": line[6], "Login Time": line[37], \
+        "Altitude": line[7], "Ground Speed": line[8], "Heading": line[38], \
+        "Route": line[30], "Remarks": line[29], "Planned Aircraft": line[9], \
+        "Planned Departure Airport": line[11], "Planned Altitude": flightlevel_to_feet(line[12]), \
+        "Planned Destination Airport": line[13], "Flight Type": line[21], "Planned Departure Time": \
+        line[22]}
 
 ################################################################################
 
@@ -88,65 +98,73 @@ def jsonify_data(data):
 
         #This is a voiceserver
         if len(vals) == 6:
-            # Construct dictionary of this line
-            curr_data = {"Location": vals[0], "IP Address": vals[1], "Name": vals[2],
-            "Host Name": vals[3], "Clients Allowed": vals[4]}
-            #Append to parsed data
+            # Construct dictionary of this line + append to parsed data
+            curr_data = prettify_data(vals, type="voice_servers")
             parsed_data["voice_servers"].append(curr_data)
 
         #ATC found
         elif vals[3] == "ATC":
-            #Construct dictionary of this line
-            curr_data = {"Callsign": vals[0], "Vatsim ID": vals[1], \
-            "Real Name": vals[2], "Frequency": vals[4], "Latitude": float(vals[5]), \
-            "Longitude": float(vals[6]), "Visible Range": int(vals[19]), "ATIS": \
-            vals[35], "Login Time": vals[37]}
-            #Append to parsed data
+            #Construct dictionary of this line, and append to parsed data
+            curr_data = prettify_data(vals, type="controllers")
             parsed_data["controllers"].append(curr_data)
-        elif vals[3] == "PILOT":
-            curr_data = {"Callsign": vals[0], "Vatsim ID": vals[1], "Real Name": vals[2], \
-            "Latitude": float(vals[5]), "Longitude": , float(vals[6]), "Login Time": vals[37], \
-            "Altitude": int(vals[7]), "Ground Speed":, int(vals[8]), "Heading": vals[38], \
-            "Route": vals[30], "Remarks": vals[29], "Planned Aircraft": vals[9], \
-            "Planned Departure Airport": vals[11], "Planned Altitude": flightlevel_to_feet(vals[12]), \
-            "Planned Destination Airport": vals[13], "Flight Type": vals[21]}
-            pass
 
+        #pilot found
+        elif vals[3] == "PILOT":
+            #Construct dictionary of this line
+            curr_data = prettify_data(vals, type="pilots")
+            parsed_data["pilots"].append(curr_data)
     return parsed_data
 
-#"planned_deptime", "planned_altairport"
-#   vals[22],               vals[28]
-
-
-
-
-
-
-
-
-
-
-
+################################################################################
 
 class VatsimData():
     def __init__(self):
-        self.latest_file = update_file({'time_updated': 0, 'data': None})
+        self.latest_file = {'time_updated': 0, 'data': None}
+        self.update_file()
+
+    def update_file(self):
+        '''update_file() checks for data freshness in the local cache and if
+        data is too old, then it fetches new data. It then returns freshest data'''
+        #Check cache freshness; call download if needed
+        if (self.latest_file["time_updated"] + 120) < int(time.time()):
+            #Needs updating
+            new_file = download()
+            if new_file is not None:
+                self.latest_file["time_updated"] = int(time.time())
+                #Jsonify the data
+                self.latest_file["data"] = jsonify_data(new_file)
+            else:
+                print "Downloaded file not valid"
+            #File was updated - it was too old
+            return True
+        #File wasnt actually updated because it is new enough already
+        return False
 
     def filter(self, value, **kwargs):
         return self.latest_file["data"][value]
         #return
 
+################################################################################
 
 def flightlevel_to_feet(flightlevel):
-    '''Function recieves something like 'FL360' and returns 36000'''
+    '''The flightlevel_to_feet() function recieves something like 'FL360' or 1500
+    and returns 36000 or 1500 '''
 
+    #No altitude specified, so assume it is 0
     if not(flightlevel):
         return 0
 
     flightlevel = str(flightlevel).lower()
     if "fl" in flightlevel or "f" in flightlevel:
-        return int(flightlevel.replace("fl", "").replace("f", "")) * 100
+        #Sometimes, pilots put "VFR" for altitude, so this code will execute, but
+        #Altitude is still not valid, so value error will occur
+        try:
+            return int(flightlevel.replace("fl", "").replace("f", "")) * 100
+        except ValueError:
+            return 0
     else:
+        #It could be that a number was specified (so no flight level but thousands
+        #of feet). If so, try to return it
         try:
             return int(flightlevel)
         except ValueError:
