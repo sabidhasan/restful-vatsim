3b510ba20ed1a006f303e36029aa28fe4b704b09
Abid Hasan <abidhasan@gmail.com>
2017-12-30 21:01:06 -0800

    Static functions

--- main.py
+++ main.py
@@ -67,21 +67,67 @@
 
 
 
-#do imports
+#Module imports
+from flask import Flask
+from flask_restful import Resource, Api
+import requests, random, time
 
 #create flask app
+app = Flask(__name__)
+api = Api(app)
 
-#create cache object
+#Memoization for latest data
+latest_file = {'time_updated': 0, 'file': None}
 
-#  *update* function
-    #if cache old, then call *download*
-    #return new data (either from cache or newly downloaded)
+#Generic function definitions
 
-#  *download* function
-    #randomize source
-    #download
+def get_latest_data():
+    '''get_latest_data() checks for data freshness in the local cache and if
+    data is too old, then it fetches new data. It then returns freshest data'''
+    #Check cache freshness; call download if needed
+    if (latest_file["time_updated"] + 120) < int(time.time()):
+        #Needs updating
+        new_file = download()
+        if new_file is not None:
+            latest_file["time_updated"] = int(time.time())
+            latest_file["file"] = new_file
+        else:
+            print "Downloaded file not valid"
+    #Return newest data
+    return latest_file["file"]
 
-# *flightlevel_to_feet* from cronupdater    - for parsing flight level
+
+def download():
+    '''download() tries to download the latest vatsim data from a list of
+    randomized servers'''
+    #random urls to download from
+    vatsim_urls = ["http://info.vroute.net/vatsim-data.txt", "http://data.vattastic.com/vatsim-data.txt", \
+            "http://vatsim.aircharts.org/vatsim-data.txt", "http://vatsim-data.hardern.net/vatsim-data.txt", \
+            "http://wazzup.flightoperationssystem.com/vatsim/vatsim-data.txt"]
+    random_url = random.choice(vatsim_urls)
+    #Download page
+    try:
+        data = requests.get(random_url).text
+        if not(len(data)):
+            raise ConnectionError
+    except:
+        print "Error downloading data"
+        return None
+    #Return data
+    return data
 
 
-# *routes*
+def flightlevel_to_feet(flightlevel):
+    '''Function recieves something like 'FL360' and returns 36000'''
+
+    if not(flightlevel):
+        return 0
+
+    flightlevel = str(flightlevel).lower()
+    if "fl" in flightlevel or "f" in flightlevel:
+        return int(flightlevel.replace("fl", "").replace("f", "")) * 100
+    else:
+        try:
+            return int(flightlevel)
+        except ValueError:
+            return 0
