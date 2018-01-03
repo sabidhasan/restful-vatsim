6808c2bece1236253e863a82366d7e182436e1e5
Abid Hasan <abidhasan@gmail.com>
2018-01-02 23:40:35 -0700

    Working documentation file

---
+++ docs.py
@@ -0,0 +1,6 @@
+#VOICE SERVER
+    #/api/v1/voiceServers
+    #/api/v1/voiceServers?name=London                                only servers with "london" as name
+    #/api/v1/voiceServers?name=London&exactMatch=true                match exactly
+    #/api/v1/voiceServers?limit=5                                    limit to 5
+    #/api/v1/voiceServers?forceUpdate=True                           force a cache file update
